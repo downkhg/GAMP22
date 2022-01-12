1668efcea33de2133ff14a22d1d23b6d7c66dc0f
downkhg <downkhg@gmail.com>
2022-01-12 17:47:48 +0900

    기초 문법 템플릿

---
+++ UnityBasic/Gamp22_UnityBasic/Gamp22_UnityBasic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Gamp22_UnityBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //ValMain();
+            //CritcalAttackMain();
+            StageMain();
+        }
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score:" + nScore);
+            Console.WriteLine("Rat:" + fRat);
+        }
+
+        static void CritcalAttackMain()//1.함수를 먼저 생성하고
+        {
+            Console.WriteLine("CritcalAttackMain");
+            //2.코드의 내용을 작성한다.
+        }
+
+        static void StageMain()
+        {
+
+        }
+
+        static void AttackWhile()
+        {
+            
+        }
+
+        static void MonsterListMain()
+        {
+
+        }
+    }
+}
