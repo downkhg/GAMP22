6771cd9428ab2b2e77ee436dea4e3f522b710b87
downkhg <downkhg@gmail.com>
2022-01-18 17:50:27 +0900

    플레이어 이동시키기

--- UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Dynamic.cs
+++ UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Dynamic.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Dynamic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        transform.position += Vector3.right;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Time.deltaTime;
+      
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Vector3.up * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Vector3.down * Time.deltaTime;
     }
 }
