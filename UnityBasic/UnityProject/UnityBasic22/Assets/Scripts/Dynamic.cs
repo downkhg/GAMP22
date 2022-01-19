ed02d0afa9a221c48d52bd8efc70fc93cf8156c0
downkhg <downkhg@gmail.com>
2022-01-19 17:48:36 +0900

    점프구현하기

--- UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Dynamic.cs
+++ UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Dynamic.cs
@@ -2,10 +2,14 @@
 
 public class Dynamic : MonoBehaviour
 {
+    public float JumpPower;
+    public Rigidbody2D rigidbody;
+    public bool isGround;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = this.gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,10 +21,25 @@ public class Dynamic : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position += Vector3.left * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += Vector3.up * Time.deltaTime;
+        //if (Input.GetKey(KeyCode.Space))
+        //    transform.position += Vector3.up * 3 * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.DownArrow))
-            transform.position += Vector3.down * Time.deltaTime;
+        //플레이어가 땅에 닿았을때 점프가 가능하게한다.
+        //땅에 닿았을때 변수를 참으로 설정하고,
+        //점프키를 누를때 변수를 거짓으로 설정한다.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGround == true)
+            {
+                rigidbody.AddForce(Vector3.up * JumpPower);
+                isGround = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGround = true;
+        Debug.Log("OnCollisionEnter2D:"+collision.gameObject.name);
     }
 }
