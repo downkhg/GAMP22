9eb1b400b0be7f06b78d837c703f5e2456cbf7a4
downkhg <downkhg@gmail.com>
2022-01-25 17:50:28 +0900

    총 만들기 -> 총이 아니라 공이 된 이유는?

---
+++ UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject objBullet;
+    public float ShotPower;
+
+    void Shot()
+    {
+        Rigidbody2D rigidbody = objBullet.GetComponent<Rigidbody2D>();
+        if (rigidbody != null)
+            rigidbody.AddForce(Vector3.right * ShotPower);
+        else
+            Debug.LogError(objBullet.gameObject.name + "is not Rigidbody2D");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.X))
+        {
+            Shot();
+        }
+    }
+}
