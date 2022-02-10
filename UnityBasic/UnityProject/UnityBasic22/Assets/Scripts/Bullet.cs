864a5d79726a77bea450d67b0a991104528c8c90
downkhg <downkhg@gmail.com>
2022-02-10 12:29:39 +0900

    추적 밋 물리없는 충돌체크

--- UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Bullet.cs
+++ UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
 {
     public Vector3 vStartPos;
     public float dist;
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ UnityBasic/UnityProject/UnityBasic22/Assets/Scripts/Eagle.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle : MonoBehaviour
+{
+    public GameObject objTarget;
+    public float Speed = 1;
+    public float Site = 0.5f;
+    public LayerMask colLayer;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(this.transform.position, Site);
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    int nLayer = 1 << LayerMask.NameToLayer("Player");
+    //    Vector3 vPos = this.transform.position;
+
+    //    Collider2D[] colliders =
+    //        Physics2D.OverlapCircleAll(vPos, Site, nLayer);
+
+    //    for (int i = 0; i < colliders.Length; i++)
+    //    {
+    //        Collider2D collider = colliders[i];
+
+    //        if (collider.tag == "Player")
+    //            objTarget = collider.gameObject;
+    //        else
+    //            Debug.Log("Collder["+i+"]:" + collider.gameObject.name);      
+    //    }
+    //}
+
+    private void FixedUpdate()
+    {
+        Vector3 vPos = this.transform.position;
+        //레이어를 이용하면 레이어에 해당하는 물체만 충돌체크 가능하다.
+        //int nLayer = 1 << LayerMask.NameToLayer("Player");
+        //Collider2D collider = Physics2D.OverlapCircle(vPos, Site, nLayer);
+        //레이어를 멤버변수로 만들어 섞을수도있다.
+        Collider2D collider =
+            Physics2D.OverlapCircle(vPos, Site, colLayer);
+
+        if (collider)
+        {
+            objTarget = collider.gameObject;
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (objTarget)
+        {
+            Vector3 vTargetPos = objTarget.transform.position;
+            Vector3 vPos = this.transform.position;
+            Vector3 vDist = vTargetPos - vPos;
+            Vector3 vDir = vDist.normalized;
+            float fDist = vDist.magnitude;
+
+            if(fDist > Time.deltaTime)
+                transform.position += vDir * Speed * Time.deltaTime;
+        }
+    }
+}
