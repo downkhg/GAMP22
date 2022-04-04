d76b72052ddbeb0276c83469b0da63f6efd67fbe
downkhg <downkhg@gmail.com>
2022-04-04 20:27:06 +0900

    함수의 개념

--- ProgrammingBasic/2.function/2.function/2.function.cpp
+++ ProgrammingBasic/2.function/2.function/2.function.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h> //라이브러리: 필요한 기능(함수)을 제공하는 것. 
+#include <math.h>
 //1.함수는 무엇일까? 왜 필요할까? 
 //1)계산식을 만들어 재활용한다. 
 //2)반복적인 코드를 나타면서 직관적으로 이해 할 수 있다.
 //3)매개변수를 활용하여 함수 안에 값을 전달하고, 
-//4)그 값으로 계산된 결과를 리턴을 이용하여 함수 밖으로 내보낸다.
+//  그 값으로 계산된 결과를 리턴을 이용하여 함수 밖으로 내보낸다.
 //2.함수의 선언은 왜 필요한가?
-//보기편함. 컴파일러가 위에서 부터 실행되기때문에 그함수의 기본적인 정보는 알아야만 아래 함수에서 호출가능하기때문입니다.
+//1)보기편함. 
+//2)컴파일러가 위에서 부터 실행되기때문에 그함수의 기본적인 정보는 알아야만 아래 함수에서 호출가능하기때문입니다.
 
 //라이브러리함수: 사용자가 정의하지않고 기본으로 제공되는 함수 
-// ex) stdio.h -> scanf,printf stdlib.h -> srand, rand
+// ex) stdio.h -> scanf,printf / stdlib.h -> srand, rand
 //사용자 정의함수: 사용자가 함수의 내용을 만들어서 제공하는 함수.
 //함수의 선언: 컴파일러에게 이러한 함수가 있다는 것을 알림.
 void FunctionTestMain();
@@ -21,14 +23,16 @@ void ASCIICodeCheck(); //정의로 이동:F12
 float ArrivalTimeSec(float dist, float speed);
 char GradeCheck(int nScore);
 float Caculator(float fDataA, float fDataB, char cOp);
-
+//0. OS
 void main() //1
 {
+	printf("%f\n", pow(2, 2));
 	//F9: 브레이킹 포인트 //F5: 디버거모드로 실행
 	//F11: 함수안으로 이동 //F10: 함수단위에서 1줄 씩 이동
 	FunctionTestMain();//함수의 호출 //2
-	CaculatorLoofMain();//101
-}//109
+	CaculatorLoofMain();//102
+
+}//118
 
 //함수의 정의: 선언된함수의 내용을 만드는 것.
 //두수를 더하는 함수
@@ -48,11 +52,11 @@ float CircleArea(int rad)
 //0~128
 void ASCIICodeCheck()
 {
-	for (int i = 0; i <= 128; i++) //19 //23
+	for (int i = 0; i <= 128; i++) //18 22
 	{
-		printf("%d:%c,", i, i); //20 //24
-		if (i % 12 == 0) //21 //25 
-			printf("\n"); //22
+		printf("%d:%c,", i, i); //19 //23
+		if (i % 12 == 0) //20 //24
+			printf("\n"); //21
 	} //100
 }
 
@@ -64,11 +68,11 @@ float ArrivalTimeSec(float dist, float speed)
 }
 
 //점수를 입력받아서 학점을 돌려주는 함수
-char GradeCheck(int nScore)
+char GradeCheck(int nScore)//100
 {
 	char cGrade;//15
 
-	if (nScore >= 90)//16
+	if (nScore >= 90)//100 >= 90 -> F 16 
 	{
 		cGrade = 'A';//17
 	}
@@ -95,20 +99,20 @@ char GradeCheck(int nScore)
 //계산기 만들기
 float Caculator(float fDataA, float fDataB, char cOp)
 {
-	float fResult; //108
+	float fResult; //109
 
-	if (cOp == '+')//+ //109 //x
-		fResult = fDataA + fDataB;// 1+1 = 2 //110
-	else if (cOp == '-') //110
+	if (cOp == '+')//+ //110 //x F
+		fResult = fDataA + fDataB;
+	else if (cOp == '-') //111 //x F
 		fResult = fDataA - fDataB;
-	else if (cOp == '*') //111
+	else if (cOp == '*') //112 //x F
 		fResult = fDataA * fDataB;
-	else if (cOp == '/') //112
+	else if (cOp == '/') //113 //x F
 		fResult = fDataA / fDataB;
-	else //113
+	else //114 
 	{
 		printf("%c is not Support!\n", cOp); //x is not support!
-		return -1.0f; //리턴을 만나면 함수는 종료된다.
+		return -1.0f; //리턴을 만나면 함수는 종료된다. //115
 	}
 
 	return fResult; //111
@@ -124,19 +128,19 @@ float Caculator(float fDataA, float fDataB, char cOp)
 //		    두수를 사칙연산하는 함수를 호출하고 그결과를 출력한다.
 void CaculatorLoofMain()
 {
-	float fDataA, fDataB, fResult; //두값을 입력하는 실수형변수와 정답을 입력하는 변수를 만들었다. 초기화 되지않았으므로 모든 변수의 값은 알수없다. //102
-	char cOp = 0; //연산자를 입력받기위해서 정수형변수를 선언하고 초기화지 않았다. //103
+	float fDataA, fDataB, fResult; //두값을 입력하는 실수형변수와 정답을 입력하는 변수를 만들었다. 초기화 되지않았으므로 모든 변수의 값은 알수없다. //103
+	char cOp = 0; //연산자를 입력받기위해서 정수형변수를 선언하고 초기화지 않았다. //104
 
-	while (cOp != 'x') //104
+	while (cOp != 'x') //105 //117 x T
 	{
-		printf("ex) 1+1 exit)?x?>> "); //105
-		scanf("%f%c%f", &fDataA, &cOp, &fDataB); //106 //1+1
+		printf("ex) 1+1 exit)?x?>> "); //106
+		scanf("%f%c%f", &fDataA, &cOp, &fDataB); //107 //1x1
 
-		fResult = Caculator(fDataA, fDataB, cOp); //107 
+		fResult = Caculator(fDataA, fDataB, cOp); //108
 
-		printf("%f%c%f=%f\n", fDataA, cOp, fDataB, fResult); //108 
+		printf("%f%c%f=%f\n", fDataA, cOp, fDataB, fResult); //116
 	}
-}
+} //118
 
 void FunctionTestMain()
 {
@@ -147,4 +151,4 @@ void FunctionTestMain()
 	printf("Score:100 Gread:%c\n", GradeCheck(100));//14
 	//특정 내용을 출력해주고 끝나는 함수.
 	ASCIICodeCheck(); //17
-}
+} //101
