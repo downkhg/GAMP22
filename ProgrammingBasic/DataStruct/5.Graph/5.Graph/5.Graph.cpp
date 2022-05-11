d6f91bebc3abd0319deedd530bb6119e09858018
downkhg <downkhg@gmail.com>
2022-05-11 18:18:41 +0900

    그래프기본예제

---
+++ ProgrammingBasic/DataStruct/5.Graph/5.Graph/5.Graph.cpp
@@ -0,0 +1,85 @@
+/*##################################
+이진트리(C언어 수업용)
+파일명: Graph.cpp
+작성자 : 김홍규(downkhg@gmail.com)
+마지막수정날짜 : 2022.05.11
+버전 : 1.01
+###################################*/
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+//노드에 멤버는 필요에 따라 추가 할수있다.
+struct SNode {
+	char cData;
+	list<SNode*> listAdj;
+	bool bVisit;
+};
+
+SNode* CreateNode(char data)
+{
+	SNode* pTemp = new SNode;
+	pTemp->cData = data;
+	pTemp->bVisit = false;
+	return pTemp;
+};
+
+bool MakeAdjcency(SNode* pParent, SNode* pChilde)
+{
+	if (pParent == NULL)//0x01 == NULL -> F
+		return false;
+	pParent->listAdj.push_back(pChilde);
+	return true;
+};
+//이 방문방법은 BFS에 가깝지만 문제가있음.
+void Traverse(SNode* pNode)
+{
+	if (pNode == NULL) return; //N == N -> T
+	//Traverse(pNode);
+	if (pNode->bVisit == false)
+	{
+		printf("%c\n", pNode->cData);
+		list<SNode*>::iterator  it;
+		for (it = pNode->listAdj.begin(); it != pNode->listAdj.end(); it++)
+		{
+			SNode* pAdj = *it;
+			Traverse(pAdj);
+		}
+		pNode->bVisit = true;
+	}
+}
+
+void Print(SNode* pSeed)
+{
+	Traverse(pSeed);
+}
+
+enum E_NODE { A, B, C, D, E, F, G,H,MAX };
+
+void main()
+{
+	SNode* pNodes[MAX];
+	char cNodeData = 'A';
+	for (int i = 0; i < MAX; i++)
+	{
+		pNodes[i] = CreateNode(cNodeData);
+		cNodeData++;
+	}
+	
+	MakeAdjcency(pNodes[A], pNodes[B]);
+	MakeAdjcency(pNodes[B], pNodes[D]);
+	MakeAdjcency(pNodes[B], pNodes[F]);
+	MakeAdjcency(pNodes[C], pNodes[B]);
+	MakeAdjcency(pNodes[D], pNodes[H]);
+	MakeAdjcency(pNodes[E], pNodes[C]);
+	MakeAdjcency(pNodes[E], pNodes[H]);
+	MakeAdjcency(pNodes[F], pNodes[D]);
+	MakeAdjcency(pNodes[F], pNodes[G]);
+	MakeAdjcency(pNodes[F], pNodes[H]);
+	MakeAdjcency(pNodes[G], pNodes[E]);
+	MakeAdjcency(pNodes[G], pNodes[F]);
+	MakeAdjcency(pNodes[G], pNodes[H]);
+
+	Print(pNodes[A]);
+	printf("##### End Main ####");
+}
