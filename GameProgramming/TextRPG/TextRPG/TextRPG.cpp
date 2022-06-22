4520ba2a04f18378cc94c3ead13b1a171a3d3fe3
downkhg <downkhg@gmail.com>
2022-06-22 17:19:05 +0900

    상점 추가하기

--- GameProgramming/TextRPG/TextRPG/TextRPG.cpp
+++ GameProgramming/TextRPG/TextRPG/TextRPG.cpp
@@ -83,6 +83,7 @@ class Player {
 	Status m_sStatus;
 	int m_nLv = 1;
 	int m_nExp = 0;
+	int m_nGold;
 
 	vector<Item> m_listIventory;
 	vector<Item> m_listEqument;
@@ -122,10 +123,30 @@ public:
 		SetIventory(cItem);
 	}
 
-	Player(const char* strName = "NONE", int _hp = 100, int _mp = 100, int _str = 20, int  _int = 10, int _def = 10, int _exp = 0, int equmentCount = 3)
+	bool Buy(Player& target, int idx)
+	{
+		Item item = target.GetIventoryIdx(idx);
+		if (item.nGold <= m_nGold)
+		{
+			SetIventory(item);
+			m_nGold -= item.nGold;
+			return true;
+		}
+		return false;
+	}
+
+	void Sell(int idx)
+	{
+		Item item = GetIventoryIdx(idx);
+		DeleteIventory(idx);
+		m_nGold += item.nGold;
+	}
+
+	Player(const char* strName = "NONE", int _gold = 0, int _hp = 100, int _mp = 100, int _str = 20, int  _int = 10, int _def = 10, int _exp = 0, int equmentCount = 3)
 	{
 		Set(strName, _hp, _mp, _str, _int, _def, _exp);
 		m_listEqument.resize(equmentCount);
+		m_nGold = _gold;
 	}
 
 	void Set(string strName, int _hp, int _mp, int _str, int _int, int _def, int _exp)
@@ -186,6 +207,7 @@ public:
 		cout << "######### Equment ######### " << endl;
 		for (int i = 0; i < m_listEqument.size(); i++)
 			cout << i << ":" << m_listEqument[i].strName << endl;
+		cout << "######### Gold:" << m_nGold << " ######### " << endl;
 	}
 };
 
@@ -217,9 +239,21 @@ public:
 void main()
 {
 	//공격? 주인공이 게임에서 상대를 쓰러뜨리려고하는 행위
-	Player cPlayer("Player");
+	Player cPlayer("Player",100000);
 	Player cMonster("Monster");
 	ItemManager cItemManager;
+	Player cShop;
+
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::WOOD_SOWRD));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::WOOD_ARMOR));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::WOOD_RING));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::BONE_SOWRD));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::BONE_AMROR));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::BONE_RING));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::HP_POTION));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::MP_POTION));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::STONE));
+	cShop.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::BOOM));
 
 	//cMonster.SetIventory(Item(Item::ETC, "HPPotion", "Hp Recover", Status(100, 0, 0, 0, 0), 100));
 	Item cHPPotion = cItemManager.GetItem(ItemManager::E_ITEM_LIST::HP_POTION);
@@ -227,13 +261,14 @@ void main()
 
 	cPlayer.SetIventory(cItemManager.GetItem(ItemManager::E_ITEM_LIST::BONE_AMROR));
 
-	enum E_PLACE { EXIT = -1, CRATE, IVNETORY, TOWN, FILED, BATTLE, GAME_OVER, THE_END, MAX };
+	enum E_PLACE { EXIT = -1, CRATE, IVNETORY, SHOP, TOWN, FILED, BATTLE, GAME_OVER, THE_END, MAX };
 	E_PLACE ePlace = E_PLACE::CRATE;
-	const char* strStageName[] = { "CRATE", "INVENTORY","TOWN", "FILED", "BATTLE", "GAME_OVER", "THE_END" };
+	const char* strStageName[] = { "CRATE", "INVENTORY","SHOP","TOWN", "FILED", "BATTLE", "GAME_OVER", "THE_END" };
 
 	enum E_MONSTER { SILME, SKELETON, BOSS, MON_MAX };
 	const char* strMonsterName[] = { "SILME", "SKELETON", "BOSS" };
 	int nInput;
+
 
 
 	while (ePlace != EXIT)
@@ -282,6 +317,43 @@ void main()
 				cout << i << ":" << strStageName[i] << ",";
 			cin >> nInput;
 			ePlace = (E_PLACE)nInput;
+		}
+		break;
+		case E_PLACE::SHOP:
+		{
+			cShop.Show();
+			int nInput;
+			cout << "상점입니다. 무엇을 하시겠습니까? 1: 구매, 2: 팔기, etc:마을";
+			cin >> nInput;
+			switch (nInput)
+			{
+			case 1:
+			{
+				cout << "구매할 아이템을 목록에서 선택하세요! -1:마을";
+				cin >> nInput;
+				if (nInput != -1)
+				{
+					if(cPlayer.Buy(cShop, nInput))
+						cout << "구매: "<< nInput << endl;
+					else
+						cout << "구매 실패!" << endl;
+				}
+			}
+			break;
+			case 2:
+			{
+				cPlayer.Show();
+				cout << "판매할 아이템을 목록에서 선택하세요! -1:마을";
+				cin >> nInput;
+				if (nInput != -1)
+				{
+					cPlayer.Sell(nInput);
+					cout << "판매: " << nInput << endl;
+				}
+			}
+			break;
+			}
+			ePlace = E_PLACE::TOWN;	
 		}
 		break;
 		case FILED:
