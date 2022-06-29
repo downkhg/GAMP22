0efe29c6573f2346cb77065e270ab24e58d05f2a
downkhg <downkhg@gmail.com>
2022-06-29 18:17:03 +0900

    스테이트패턴 풀이

--- ProgrammingBasic/5.DesignPettern/4.State/4.State/State.cpp
+++ ProgrammingBasic/5.DesignPettern/4.State/4.State/State.cpp
@@ -1,106 +1,44 @@
+#include "State.h"
 #include <iostream>
-#include <stdlib.h> //메모리 동적할당 헤더
-#include <crtdbg.h> //메모리 누수 탐지 헤더
 
-class Context;
-class StateOne;
-class StateTow;
-class StateThree;
-
-
-
-class Context
+void StateThree::GoNext(Context* context)
 {
-	State* m_pState = NULL;
-	///※그러나 friend는 사용하지않도록 권장함.
-	//다음과 같이 friend클래스를 활용하여
-	//SetState를 외부에서는 사용하지 못하지만,
-	//각 State객체에서는 접근가능하도록 할수도 있다.
-	friend class StateOne;
-	friend class StateTow;
-	friend class StateThree;
+	context->SetState(new StateOne);
+}
 
-	void SetState(State* newState)
-	{
-		m_pState = newState;
-	}
-
-public:
-	Context()
-	{
-		m_pState = new StateOne();
-	}
-
-	Context(State* curState)
-	{
-		m_pState = curState;
-	}
-
-	~Context()
-	{
-		delete m_pState;
-	}
-	void GoNext()
-	{
-		m_pState->GoNext(this);
-	}
-};
-
-class State
+void StateTow::GoNext(Context* context) 
 {
-	friend class Context;
-public:
-	virtual void GoNext(Context* context) = 0;
-};
+	context->SetState(new StateThree);
+}
 
-
-class StateThree : public State
+void StateOne::GoNext(Context* context)
 {
-public:
-	void GoNext(Context* context) override
-	{
-		context->SetState(new StateOne);
-	}
-};
+	context->SetState(new StateTow);
+}
 
-class StateTow : public State
+Context::Context()
 {
-public:
-	void GoNext(Context* context) override
-	{
-		context->SetState(new StateTow);
-	}
-};
+	m_pState = new StateOne();
+}
 
-
-class StateOne : public State
+Context::Context(State* state)
 {
-public:
-	void GoNext(Context* context) override
-	{
-		context->SetState(new StateThree);
-	}
-};
+	m_pState = state;
+}
 
-using namespace std;
-
-void main()
+Context::~Context()
 {
-	//_CrtSetBreakAlloc(165); //메모리 누수시 번호를 넣으면 할당하는 위치에 브레이크 포인트를 건다.
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF); //메모리 누수 검사 
+	delete m_pState;
+}
 
-	Context cContext;
-	//성성자에서 기본 StateOne을 할당하거나,
-	//성성시 State를 설정할수있도록 생성자를 정의함.
-	//소멸자에는 생성된 객체를 소멸시키도록 만듦.
+void Context::SetState(State* newState)
+{
+	if (m_pState) delete m_pState;
+	std::cout << "SetState:" << typeid(*newState).name() << std::endl;
+	m_pState = newState;
+}
 
-	///※그러나 friend는 사용하지않도록 권장함.
-	//friend키워드를 이용하여 Context만 사용가능하게 만들 수도 있다.
-	//다음인터페이스는 사용하기 불편함
-	//cContext.SetState(new StateOne());
-	//Context::GoNext()시 다른 생성된 객체가 삭제되도록,
-	//Contest::SetState()에서 기존에 할당된 객체를 삭제하게 만듦.
-	cContext.GoNext();
-	cContext.GoNext();
-	cContext.GoNext();
+void Context::GoNext()
+{
+	m_pState->GoNext(this);
 }
